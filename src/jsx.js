34e4a5748a28a7ca69ea7283334a3643933bcec4
Brian Hon <brianwhon@gmail.com>
2019-01-26 16:39:16 -0500

    removed contents from context object that marlon had for testing purposes. object is now empty

--- src/jsx.js
+++ src/jsx.js
@@ -2,11 +2,7 @@ import { StaticRouter } from 'react-router-dom';
 
 const reactRouter = CompiledApp => {
     let componentRoute = req.url;
-    const context = { 
-        data: {}, 
-        head: [],
-        req
-    }
+    const context = {};
     const jsx = (
         <StaticRouter context={context} location={componentRoute}>
             <CompiledApp />
